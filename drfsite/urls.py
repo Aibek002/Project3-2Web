e73580fbeb256a064794f6badf17979afa9cdc62
Aibek <200103346@stu.sdu.edu.kz>
2022-05-04 19:21:02 +0600

    first commit

--- drfsite/urls.py
+++ drfsite/urls.py
@@ -4,19 +4,11 @@ from django.urls import path, include
 from women.views import *
 
 urlpatterns = [
-    path('contact/', contact_view, name='contact'),
-    path('success/', success_view, name='success'),
-    path('', home, name='home'),
-    path('addpage/', addpage, name='add_page'),
-    path("upload", entry, name='addbeast'),
-    path('/<slug:post_slug>/', show_post, name='post'),
-    path('admin/', admin.site.urls),
-    path('women/', WomenAPIList.as_view()),
-    path('register',registerPage,name='register'),
-    path('login',loginPage,name='login'),
-    path('logout',logout,name='logout'),
-    path('<int:pk>/update', update.as_view(), name='update'),
-    path('<slug:slug>/',delete,name='delete'),
-    path('women/<int:pk>/', WomenAPIUpdate.as_view()),
-    path('womendelete/<int:pk>/', WomenAPIDestroy.as_view()),
+    path('', index4, name='home'),
+    path('page1', index1, name='page1'),
+    path('page2', index2, name='page2'),
+    path('page3', index3, name='page3'),
+    path('page4', index, name='page4'),
+
+
 ]
